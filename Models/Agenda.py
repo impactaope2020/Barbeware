a4e4c3ead09dc3c1e120c2072df9afe9b3639255
impactaope2020 <wilson.silva@aluno.faculdadeimpacta.com.br>
2020-09-27 15:09:17 -0300

    teste

--- Models/Agenda.py
+++ Models/Agenda.py
@@ -1,29 +1,34 @@
-from datetime import datetime, timedelta
 import sqlite3
 
 class Agenda:
-    with sqlite3.connect('Agenda.db') as conn:
+    with sqlite3.connect("Agenda.db") as conn:
         cursor = conn.cursor()
         cursor.execute("""Create table if not exists Agenda(
-            id integer primary key autoincrement,
-            nome_cliente varchar(20),
-            sobrenome_cliente varchar(40),
-            email_cliente varchar(50),
-            horario_marcado varchar(15)
-        )""")
-    
-    def Agendamento(nome_cliente, sobrenome_cliente, email_cliente, horario_marcado):
-        with sqlite3.connect('Agenda.db') as conn:
+                            id integer primary key autoincrement not null,
+                            agenda_codcliente integer key not null,
+                            agenda_codbarbeiro integer not null,
+                            agenda_data date not null,
+                            agenda_horario time not null)""")
+        
+    def  AgendarCliente(agenda_codcliente,  agenda_codbarbeiro, agenda_data,  agenda_horario):
+        with sqlite3.connect("Agenda.db") as conn:
             cursor = conn.cursor()
-            cursor.execute("""insert into Agenda (nome_cliente, sobrenome_cliente, email_cliente, horario_marcado)
-                            values (?, ?, ?, ?)""", (nome_cliente, sobrenome_cliente, email_cliente, horario_marcado))
-    
-    def ConfigAgenda(horario_funcionamento, horario_fechamento, tempo_corte):
-        horario_funcionamento = datetime.strptime(horario_funcionamento, "%H:%M")
-        horario_fechamento = datetime.strptime(horario_fechamento, "%H:%M")
-        config = [str(horario_funcionamento)[11:]]
-        while horario_funcionamento < horario_fechamento and horario_funcionamento + timedelta(minutes=tempo_corte) < horario_fechamento:
-            soma_horas = horario_funcionamento + timedelta(minutes=tempo_corte)
-            horario_funcionamento = horario_funcionamento + timedelta(minutes=tempo_corte)
-            config.append(str(soma_horas)[11:])
-        return config
+            cursor.execute(""" insert into Agenda(agenda_codcliente, agenda_codbarbeiro, agenda_data, agenda_horario) values(?, ?, ?, ?)""", (agenda_codcliente, agenda_codbarbeiro, agenda_data, agenda_horario))
+            conn.commit
+            
+    def ListarAgendamento():
+        with sqlite3.connect("Agenda.db") as conn:
+            cursor = conn.cursor()
+            return cursor.execute("select * from Agenda")
+        
+    def ExcluirAgendamento(id):
+        with sqlite3.connect("Agenda.db") as conn:
+            cursor = conn.cursor()
+            cursor.execute("Delete from Agenda where id = ? ", (id, ))
+            conn.commit()
+
+    def Reagendamento(agenda_codcliente, agenda_codbarbeiro, agenda_data, agenda_horario):
+        with sqlite3.connect("Agenda.db") as conn:
+            cursor = conn.cursor()
+            cursor.execute(""" Update Agenda set agenda_data = ?, agenda_horario = ?
+                                                where id = ? """, (agenda_codcliente, agenda_codbarbeiro, agenda_data, agenda_horario, id))
