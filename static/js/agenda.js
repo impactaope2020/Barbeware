b763f466673317ef9308b99dad9bbdefb6d3318f
kaique14 <lelis423@gmail.com>
2020-09-28 15:54:58 -0300

    Agenda 0.2

--- static/js/agenda.js
+++ static/js/agenda.js
@@ -2,6 +2,27 @@ var barbeiro = document.getElementById("barbeiro");
 var horarios = document.getElementById("hora");
 var datas = document.getElementById("data")
 
+
+var currentDate = new Date();
+
+var date = currentDate.getDate();
+var month = currentDate.getMonth(); 
+var year = currentDate.getFullYear();
+
+var dateString = year + "-" + pad(month + 1) + "-" + date;
+
+datas.value = dateString;
+
+function pad(n){
+    if (n < 10){
+        return "0" + n;
+    } 
+    else{
+        return n
+    }
+}
+
+
 barbeiro.onchange = function(){
 
     fetch('Agendamento/' + barbeiro.value + "/" + datas.value) .then(function(response){
@@ -11,7 +32,6 @@ barbeiro.onchange = function(){
            optionHTML = '';
             for (disponiveis = 0; disponiveis < data[1].horarios_disponiveis.length; disponiveis++){
                 optionHTML += '<option value="' + data[1].horarios_disponiveis[disponiveis] +'">' + data[1].horarios_disponiveis[disponiveis] + '</option>'
-                console.log(data[1].horarios_disponiveis[disponiveis])
                 horarios.innerHTML = optionHTML;
             }
             if (data[1].horarios_disponiveis.length === 0){
--- teste.py
+++ teste.py
@@ -1,23 +1,9 @@
 from Models.Agenda import Agenda
 from Models.ConfigAgenda import ConfigAgenda
+from Models.Usuarios import Usuario
 import datetime 
 import sqlite3
 
 
-Agenda.Agendamento(1, 2, "2020-09-18", "12:00:00", 1)
-
-
-agenda =  Agenda.ReturnHorarios(2, "2020-09-18")
-
-agenda_marcada = []
-for agendados in agenda:
-    agenda_marcada.append(agendados)
+print(Usuario.RetornarBarbeiroId("Selecione um barbeiro"))
     
-print(agenda_marcada[4])
-
-agenda_disponivel = []
-for todos in ConfigAgenda.RetornarHorarios():
-    if todos[3] not in agenda_marcada[4]:
-        agenda_disponivel.append(todos)
-
-print(agenda_disponivel)
