6ffdd682efba08fed7ccf27818cad2a52776c1e1
kaique1999 <kaique_lelis@hotmail.com>
2020-11-23 08:28:09 -0300

    Criar Pedido

--- teste.py
+++ teste.py
@@ -1,22 +1,27 @@
 from Models.Agenda import Agenda
 from Models.ConfigAgenda import ConfigAgenda
 from Models.Usuarios import Usuario
+from Models.EntradaEstoque import EntradaEstoque
+from Models.Client import Cliente
+from Models.Produtos import Produtos
 import datetime 
 import sqlite3
 
 
-<<<<<<< HEAD
-texto = """A """
 
-qtd = 0
-for caracteres in texto:
-    qtd += 1
-print(qtd)
-=======
-for agendamentos in Agenda.SelectAgendamentos('2020-09-20', 2):
-    print(agendamentos)
->>>>>>> 7aa8ac34c7c5ed5684c487667cf36a99300d00b5
-    
-
-'''for agendamento in Agenda.ReturneAgendamentos():
-    print(agendamento)'''
+"""Cadastrar Produtos  """
+Produtos.CadastrarProdutos('Navalha', '1,00')
+Produtos.CadastrarProdutos('Gel', '10,00')
+Produtos.CadastrarProdutos('Pomada Modeladora', '10,00')
+Produtos.CadastrarProdutos('Laque', '15,00')
+Produtos.CadastrarProdutos('Alisante', '45,00')
+Produtos.CadastrarProdutos('Shampoo', '11,00')
+Produtos.CadastrarProdutos('Condicionador', '11,00')
+"""Cadastrar Clientes"""
+Cliente.CadastrarCliente('Kaique', 'Lelis', 'kaique_lelis@hotmail.com', 'Rua Riga', '370', '','SÂO PAULO', 'SP', '04249-070')
+Cliente.CadastrarCliente('Wilson', 'Alves', 'wilson.alves@hotmail.com', 'Rua Viera', '29', '','SÂO PAULO', 'SP', '04242-170')
+Cliente.CadastrarCliente('Matheus', 'Lins', 'matheus_lins@hotmail.com', 'Rua Souza', '30', '','SÂO PAULO', 'SP', '04249-070')
+Cliente.CadastrarCliente('Kaique', 'Lelis', 'Kaique_lelis@hotmail.com', 'Rua Riga', '370', '','SÂO PAULO', 'SP', '04249-070')
+Cliente.CadastrarCliente('Kaique', 'Lelis', 'Kaique_lelis@hotmail.com', 'Rua Riga', '370', '','SÂO PAULO', 'SP', '04249-070')
+Cliente.CadastrarCliente('Kaique', 'Lelis', 'Kaique_lelis@hotmail.com', 'Rua Riga', '370', '','SÂO PAULO', 'SP', '04249-070')
+Cliente.CadastrarCliente('Kaique', 'Lelis', 'Kaique_lelis@hotmail.com', 'Rua Riga', '370', '','SÂO PAULO', 'SP', '04249-070')
